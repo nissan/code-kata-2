8144b71b58da6b50080b7ac48ccf9d9dc38f4f28
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2023-12-11 02:52:27 +1100

    feat: mocks the decision engine expected data format

---
+++ external/decision-engine/main.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class DEForm(BaseModel):
+    name: str
+    yearEstablished: int
+    summaryProfitOrLoss: float
+    preAssessment: int
+
+app = FastAPI()
+
+@app.post("/")
+async def root(application:DEForm):
+    return {"message": "Loan Processed and Approved"}
